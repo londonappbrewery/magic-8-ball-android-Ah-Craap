f68f4446ebe416b4aacef93d8fcc079e6873d0e2
Prachurya <upendradeka86@gmail.com>
2018-04-04 11:56:54 +0530

    Hey Philipp , i have completed the project. Hope you like it!

--- app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
@@ -14,10 +14,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        final ImageView ballDisplay=(ImageView)findViewById(R.id.image_eightBall);
-        final int []  ballArray =
+
+        final ImageView ballDisplay= findViewById(R.id.image_eightBall);
+        final int []  ballArray =new int[]
                 {R.drawable.ball1,R.drawable.ball2,R.drawable.ball3,R.drawable.ball4,R.drawable.ball5};
-        Button myButton =(Button)findViewById(R.id.askButton);
+        Button myButton = findViewById(R.id.askButton);
         myButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
